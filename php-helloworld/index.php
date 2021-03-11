12b1e089804ed6b3ac98fe40c9e9c3624a59b478
lscalabrini <53845119+lscalabrini@users.noreply.github.com>
2021-03-11 13:20:01 -0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,7 +1,7 @@
 <?php
 print "Sitio web de prueba. Versión de PHP: " . PHP_VERSION . "\n";
 ?>
-<h1> Version 3.0 Modificado para Telecom.  La solicitud está siendo procesada por el pod:   
+<h1>  La solicitud está siendo procesada por el pod:   
  
 <?php
 $ip = getenv('HOSTNAME');
