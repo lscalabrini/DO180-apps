2eb08d52899aa9d9908e5e954d23577dbcd660e8
lscalabrini <53845119+lscalabrini@users.noreply.github.com>
2021-04-08 18:38:44 -0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,8 @@ print "Sitio web de prueba. Versión de PHP: " . PHP_VERSION . "\n";
 ?>
 <h1>  La solicitud está siendo procesada por el pod:   
  
+ MODIFICACION VERSION 2.0 
+ 
 <?php
 $ip = getenv('HOSTNAME');
 
